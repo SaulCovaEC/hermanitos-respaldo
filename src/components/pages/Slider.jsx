81673a2bf7980553ec8ba3bd80621678ef464b4d
Saul Cova <32908858+SaulCovaEC@users.noreply.github.com>
2020-03-03 18:40:36 -0400

    .

--- src/components/pages/Slider.jsx
+++ src/components/pages/Slider.jsx
@@ -36,7 +36,7 @@ export default class Slider extends Component {
       <div id="slider">
         <div className="slide fade" style={{display: (this.state.slideIndex === 1) ? "block" : "none"}}>
           <div className="numbertext">1 / {this.state.slideLength}</div>
-          <img src="./img/img-3.jpg" className="slider-back" alt="Slider 1"/>
+          <img src="./img/img-13.jpg" className="slider-back" alt="Slider 1"/>
         </div>
 
         <div className="slide fade" style={{display: (this.state.slideIndex === 2) ? "block" : "none"}}>
@@ -49,44 +49,39 @@ export default class Slider extends Component {
           <img src="./img/img-6.jpg" className="slider-back" alt="Slider 3"/>
         </div>
 
-        <div className="slides fade" style={{display: (this.state.slideIndex === 3) ? "block" : "none"}}>
+        <div className="slides fade" style={{display: (this.state.slideIndex === 4) ? "block" : "none"}}>
           <div className="numbertext">4 / {this.state.slideLength}</div>
           <img src="./img/img-10.jpg" className="slider-back" alt="Slider 4"/>
         </div>
 
-        <div className="slides fade" style={{display: (this.state.slideIndex === 3) ? "block" : "none"}}>
+        <div className="slides fade" style={{display: (this.state.slideIndex === 5) ? "block" : "none"}}>
           <div className="numbertext">5 / {this.state.slideLength}</div>
           <img src="./img/img-11.jpg" className="slider-back" alt="Slider 5"/>
         </div>
 
-        <div className="slides fade" style={{display: (this.state.slideIndex === 3) ? "block" : "none"}}>
+        <div className="slides fade" style={{display: (this.state.slideIndex === 6) ? "block" : "none"}}>
           <div className="numbertext">6 / {this.state.slideLength}</div>
-          <img src="./img/img-13.jpg" className="slider-back" alt="Slider 6"/>
+          <img src="./img/img-7.jpg" className="slider-back" alt="Slider 6"/>
         </div>
 
-        <div className="slides fade" style={{display: (this.state.slideIndex === 3) ? "block" : "none"}}>
+        <div className="slides fade" style={{display: (this.state.slideIndex === 7) ? "block" : "none"}}>
           <div className="numbertext">7 / {this.state.slideLength}</div>
           <img src="./img/img-12.jpg" className="slider-back" alt="Slider 7"/>
         </div>
 
-        <div className="slides fade" style={{display: (this.state.slideIndex === 3) ? "block" : "none"}}>
+        <div className="slides fade" style={{display: (this.state.slideIndex === 8) ? "block" : "none"}}>
           <div className="numbertext">8 / {this.state.slideLength}</div>
           <img src="./img/img-9.jpg" className="slider-back" alt="Slider 8"/>
         </div>
 
-        <div className="slides fade" style={{display: (this.state.slideIndex === 3) ? "block" : "none"}}>
+        <div className="slides fade" style={{display: (this.state.slideIndex === 9) ? "block" : "none"}}>
           <div className="numbertext">9 / {this.state.slideLength}</div>
           <img src="./img/img-8.jpg" className="slider-back" alt="Slider 9"/>
         </div>
 
-        <div className="slides fade" style={{display: (this.state.slideIndex === 3) ? "block" : "none"}}>
+        <div className="slides fade" style={{display: (this.state.slideIndex === 10) ? "block" : "none"}}>
           <div className="numbertext">10 / {this.state.slideLength}</div>
           <img src="./img/img-2.jpg" className="slider-back" alt="Slider 10"/>
-        </div>
-
-        <div className="slides fade" style={{display: (this.state.slideIndex === 3) ? "block" : "none"}}>
-          <div className="numbertext">10 / {this.state.slideLength}</div>
-          <img src="./img/img-7.jpg" className="slider-back" alt="Slider 3"/>
         </div>
       </div>
     );
